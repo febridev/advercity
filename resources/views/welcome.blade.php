e847f49efca8695bfa622bfe224f9c136c237d6e
abidin13 <febriyantabidin@gmail.com>
2015-05-24 23:27:15 +0700

    commit ketiga versi 1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,22 +4,21 @@
 	<meta charset="UTF-8">
 	<title>Advercity</title>
 	<link rel="stylesheet" href="{{asset('/font-awesome/css/font-awesome.min.css')}}">
+	<link rel="stylesheet" href="{{asset('/bootstrap/dist/css/bootstrap.min.css')}}">
 	<script src="{{asset('/jquery/jquery.min.js')}}" type="text/javascript"></script>
 	<script src="{{asset('/onepagescroll/jquery.onepage-scroll.min.js')}}" type="text/javascript"></script>
 	<link rel="stylesheet" href="{{asset('/css/style.css')}}">
 	<link rel="stylesheet" href="{{asset('/onepagescroll/onepage-scroll.css')}}">
 </head>
 <header>
-	<h1><a href="#">Advercity</a></h1>
+	<h1><a href="#"><b>Adver</b>city</a></h1>
 </header>
 <body>
 	<div class="main">
 		<section class="one">
 			<div class="page-container">
-				<h2>Advercity</h2>
-				<p>Where are you,</p>
-				<p>is many event</p>
-				<i class="fa fa-camera-retro fa-lg"></i>
+				<h2>Login</h2>
+				<p>Ini berisikan untuk form login dan beberapa link yang di butuhkan</p>
 			</div>
 		</section>
 		
